b2d1def675c27c9da7123adbefaa4904acb1bf34
dev <vadimbert@gmail.com>
2020-03-11 18:41:32 +0200

    Commit work 11/03/2020

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ class Configuration(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root@localhost/flaskblog'
-    CKEDITOR_PKG_TYPE = 'standart'
+    #CKEDITOR_PKG_TYPE = 'standart'
     CKEDITOR_SERVE_LOCAL = False
     CKEDITOR_FILE_UPLOADER = 'upload'
     SECRET_KEY = 'dev key'
